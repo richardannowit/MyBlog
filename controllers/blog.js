7a805051bb5ba2b64489e43c4c7175f8a1279240
richardannowit <richardannowit@gmail.com>
2020-06-13 21:30:00 +0700

    Add ckeditor

--- controllers/blog.js
+++ controllers/blog.js
@@ -1,3 +1,9 @@
+const BlogPost = require('../models/BlogPost');
+const CategoryPost = require('../models/CategoryPost');
+
 module.exports = (req,res)=>{
-	res.render('blog');
+	BlogPost.find({},(err,post)=>{
+		res.render('blog',{blogpost: post});
+	})
+	
 }
