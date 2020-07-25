fa9d80e7997cae8bf519e475fd1a56d3f49908e5
richardannowit <richardannowit@gmail.com>
2020-07-25 11:44:11 +0700

    Change Schema of Category Blog

--- models/CategoryPost.js
+++ models/CategoryPost.js
@@ -2,11 +2,6 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const SchemaCategoryPost = new Schema({
-	short_name: {
-		type: 'String',
-		required: true,
-		unique: true
-	},
 	name: {
 		type: 'String',
 		required: true,
